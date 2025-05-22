a793cd29718f4687e6a3751c8a59ebb292cf3cbd
Vaishnavi-M-Patil <153639596+Vaishnavi-M-Patil@users.noreply.github.com>
2025-05-22 16:57:47 +0530

    Create main.tf

---
+++ state-locking/developer1/main.tf
@@ -0,0 +1,22 @@
+#terraform block
+terraform {
+  backend "s3" {
+    bucket="terraform-backend-1283"                 # bucket name
+    region = "us-east-1"                            #bucket region
+    key = "backend/terraform.tfstate"               #path of state file on s3
+    dynamodb_table = "state-locking-table"          #dynamodb table name
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2-instance" {
+    ami = "ami-0953476d60561c955"
+    instance_type = "t2.micro"
+    key_name = "vir-key"
+    tags = {
+      Name = "ec2 instance - developer1"
+    }
+}
